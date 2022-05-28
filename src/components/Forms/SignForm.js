ff2cbbebd20f5e4fb8ac0b551437a4e09081e33a
vozoul <vozoul@gmail.com>
2022-05-28 11:47:05 +0200

    WIP - add forms and behaviors

--- src/components/Forms/SignForm.js
+++ src/components/Forms/SignForm.js
@@ -1,14 +1,23 @@
-import {useState} from 'react'
+import {useState, useContext} from 'react'
+import { AppContext } from 'services/contexts/AppContext';
 import {SwitchButton} from 'components'
 import "./SignForm.css"
 
 const SignForm = () => {
 
+    const {toggleModal} = useContext(AppContext)
+
     const [signIn, setSignIn] = useState(false)
     const toggleSign = () => setSignIn(!signIn)
 
+    const submit = (e) => {
+        e.preventDefault()
+        console.log('submitted')
+        toggleModal()
+    };
+
     return (
-        <form className="" onSubmit={() => {}}>
+        <form className="" onSubmit={submit}>
             <h3>{(signIn)? "Connection" : "Inscription"}</h3>
             <div className='on-off-choice'>
                 <div>Sign-up</div>
